233440c01c1389c148950323e1108a10e6bb4654
Samuel Olmedo Ortiz <90302812+pagedhack@users.noreply.github.com>
2022-12-12 20:03:45 -0600

    Update usuario.js

--- cafeteria/log/js/usuario.js
+++ cafeteria/log/js/usuario.js
@@ -1,7 +1,7 @@
         var bConsultar = document.getElementById("consultar")
 
         bConsultar.addEventListener("click", function () {
-            axios.get("https://cafesus-despliegue-production-1e7c.up.railway.app/usuario")
+            axios.get("https://cafesus-despliegue-production-fd9e.up.railway.app/usuario")
                 .then(function (response) {
                     console.log(response);
                     tabla(response.data)
